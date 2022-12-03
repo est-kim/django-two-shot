31b3b3993e15bc32f29edc6b7a42330c66830ae7
Esther Kim <kim.esther117@gmail.com>
2022-12-02 16:27:24 -0800

    Feature 10 Complete

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from receipts.views import receipt_list
-from accounts.views import user_login, user_logout
+from accounts.views import user_login, user_logout, signup
 
 urlpatterns = [
     path("", receipt_list, name="home"),
     path("login/", user_login, name="login"),
     path("logout/", user_logout, name="logout"),
+    path("signup/", signup, name="signup"),
 ]
